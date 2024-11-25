04a14d653c54892696ad485aad1568ccb643430f
Todsapol Mangkalakeelee <ohoponza5@hotmail.com>
2024-11-25 15:31:10 +0700

    Update Function def(input_TSL).py

--- Python Beginner Practice/Function def/Function def(input_TSL).py
+++ Python Beginner Practice/Function def/Function def(input_TSL).py
@@ -1,8 +1,9 @@
 def calculate_numbers():    # กำหนดฟังชั่น และตั้งชื่อ "calculate_numbers"
     
     choice_numbers = input("โปรดป้อนตัวเลขโดยเว้นช่องว่าง คือการ เคาะ 1ที: ") # รับค่าตัวเลข
-    float_numbers = choice_numbers.split()  # สร้างตัวแปล เพื่อนำค่า input มาแยก 10, 20, 30
-    float_numbers = [float(num) for num in float_numbers] # เอาตัวแปรมาแปลงเป็น ทศนิยม และ 
+    float_numbers = choice_numbers.split()# สร้างตัวแปล เพื่อนำค่า input มาแยก 10, 20, 30
+    print(float_numbers) # แสดงผลตัวเลข Input ที่ถูกจับแยก
+    float_numbers = [float(num) for num in float_numbers] # เอาตัวแปรมาแปลงเป็น ทศนิยม และ วนลูป
     sum_numbers = sum(float_numbers)    # สร้างตัวแปลเพื่อเก็บ ค่าผลรวมของ ตัวแปร float_numbers โดยใช้ Sum
     print(f"ผลรวมก็คือ: {sum_numbers}")   # แสดงผล ผลรวม โดยดึงตัวแปร sum_numbers มาไว้ในปีกกาให้มันแสดงผล
 
